9e8a2905f536b053c75777eb6492d46ec14a103c
Rodrigo Perlin <roperlin.gok@rd.com.br>
2023-08-24 09:50:27 -0300

    feat(projet1): add text in app.tsx

--- packages/projeto1/src/App.tsx
+++ packages/projeto1/src/App.tsx
@@ -1,10 +1,9 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./themes/index";
 import Button from "./components/Button";
-import React from "react";
 const App = () => (
   <ThemeProvider theme={theme}>
-    <Button background="red">Ola</Button>
+    <Button background="red">Ola</Button>tudo bem?
   </ThemeProvider>
 );
 export default App;
