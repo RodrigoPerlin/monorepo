4c942f9da36ef1ae68f20a9f4ed0bdda6dd36d72
Rodrigo Perlin <roperlin.gok@rd.com.br>
2023-08-24 20:04:48 -0300

    fix(app1): added

--- packages/projeto1/src/App.tsx
+++ packages/projeto1/src/App.tsx
@@ -3,7 +3,7 @@ import { theme } from "./themes/index";
 import Button from "./components/Button";
 const App = () => (
   <ThemeProvider theme={theme}>
-    <Button background="red">Ola, feat</Button>
+    <Button background="red">Ola</Button>
   </ThemeProvider>
 );
 export default App;
