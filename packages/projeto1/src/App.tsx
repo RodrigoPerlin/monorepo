11c722bed5437cde6065facc85496351803a0751
Rodrigo Perlin <roperlin.gok@rd.com.br>
2023-08-24 09:53:10 -0300

    feat(projet1): remove text in app.tsx

--- packages/projeto1/src/App.tsx
+++ packages/projeto1/src/App.tsx
@@ -3,7 +3,7 @@ import { theme } from "./themes/index";
 import Button from "./components/Button";
 const App = () => (
   <ThemeProvider theme={theme}>
-    <Button background="red">Ola</Button>tudo bem?
+    <Button background="red">Ola</Button>
   </ThemeProvider>
 );
 export default App;
