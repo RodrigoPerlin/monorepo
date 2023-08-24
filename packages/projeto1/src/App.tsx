c7384f1637f76aaac5d2b0e7c9ed7e2f6fd3b9d3
Rodrigo Perlin <roperlin.gok@rd.com.br>
2023-08-24 10:06:06 -0300

    feat(projet1): add text teste 2

--- packages/projeto1/src/App.tsx
+++ packages/projeto1/src/App.tsx
@@ -3,7 +3,7 @@ import { theme } from "./themes/index";
 import Button from "./components/Button";
 const App = () => (
   <ThemeProvider theme={theme}>
-    <Button background="red">Ola</Button>
+    <Button background="red">Ola</Button>teste 2
   </ThemeProvider>
 );
 export default App;
