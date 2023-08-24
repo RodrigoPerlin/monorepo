d8f47b57f3dcce830b665eeb6db00603ee25202f
Rodrigo Perlin <roperlin.gok@rd.com.br>
2023-08-24 10:58:19 -0300

    feat(pall): add text teste 2

--- packages/projeto1/src/App.tsx
+++ packages/projeto1/src/App.tsx
@@ -3,7 +3,7 @@ import { theme } from "./themes/index";
 import Button from "./components/Button";
 const App = () => (
   <ThemeProvider theme={theme}>
-    <Button background="red">Ola</Button>
+    <Button background="red">Ola</Button>tuydo bem?
   </ThemeProvider>
 );
 export default App;
