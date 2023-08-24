d0fdf3425fd5e5b49c69f49ed7c6ce656a8d58d1
Rodrigo Perlin <roperlin.gok@rd.com.br>
2023-08-24 20:18:48 -0300

    feat(app1): asdasdsad

--- packages/projeto1/src/App.tsx
+++ packages/projeto1/src/App.tsx
@@ -3,7 +3,7 @@ import { theme } from "./themes/index";
 import Button from "./components/Button";
 const App = () => (
   <ThemeProvider theme={theme}>
-    <Button background="red">Ola</Button>
+    <Button background="red">Olassss</Button>
   </ThemeProvider>
 );
 export default App;
